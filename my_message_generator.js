77a762d5c816d799dd8033e4ec6413ac9b3500cf
grec4 <109405245+grec4@users.noreply.github.com>
2022-11-07 18:26:42 -0800

    Add code functionality

--- my_message_generator.js
+++ my_message_generator.js
@@ -1,1 +1,35 @@
-console.log('This is a wow')
+function randomGenerator(num) {
+    return Math.floor(Math.random() * num)
+}
+
+    const collectiveWords = {
+        firstWord: ["life", "nothing burger"],
+        secondWord: ["answer", "piano"], 
+        thirdWord: ["abyss", "identity", "box"]
+        }
+
+    let randomWords = []
+
+    for (let i in collectiveWords) {
+        let optionIdx = randomGenerator(collectiveWords[i].length)
+        switch(i){
+            case 'firstWord':
+                randomWords.push(collectiveWords[i][optionIdx])
+                break
+            case 'secondWord':
+                randomWords.push(collectiveWords[i][optionIdx])
+                break
+            case 'thirdWord':
+            randomWords.push(collectiveWords[i][optionIdx])
+            default:
+                randomWords.push('No info')
+        }
+    }
+
+    function newSentence(sentence) {
+        const formatted = randomWords.join('\n')
+        console.log(formatted)
+    }
+    
+    newSentence(randomWords);
+
